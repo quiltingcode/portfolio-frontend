733b13c9c658198a35a9952f84fe84013b3a00d8
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-20 08:49:11 +0000

    Create portfolio app

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Press Me</button>
+    </div>
+  );
+}
+
+export default App;

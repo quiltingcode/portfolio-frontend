68d724ffa70cb0a75dac65cf9e3d52e36c92887f
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-21 16:22:28 +0000

    Add google fonts link and create navbar styles file

--- portfolio/src/components/NavBar.component.js
+++ portfolio/src/components/NavBar.component.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavBar = () => {
     return (
-        <div>
-            navbar
-        </div>
+        <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     )
 }
 
